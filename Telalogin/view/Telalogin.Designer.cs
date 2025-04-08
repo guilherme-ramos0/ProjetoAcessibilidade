73608eb09fb49baa6c8378ee896e2e3b6f96bc3e
Guilherme <lima.guilhermeramos@gmail.com>
2025-04-08 08:55:56 -0300

    implementando tela de login e efeito de click nos textBox

--- Telalogin/view/Telalogin.Designer.cs
+++ Telalogin/view/Telalogin.Designer.cs
@@ -32,6 +32,11 @@
             this.roundedPictureBox1 = new RoundedPictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.boxCpf = new System.Windows.Forms.TextBox();
+            this.boxSenha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -59,17 +64,80 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(169, 94);
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(175, 98);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(487, 300);
+            this.pictureBox2.Size = new System.Drawing.Size(470, 289);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
+            // 
+            // boxCpf
+            // 
+            this.boxCpf.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxCpf.Location = new System.Drawing.Point(268, 197);
+            this.boxCpf.Multiline = true;
+            this.boxCpf.Name = "boxCpf";
+            this.boxCpf.Size = new System.Drawing.Size(263, 30);
+            this.boxCpf.TabIndex = 3;
+            this.boxCpf.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.boxCpf.Enter += new System.EventHandler(this.boxCpf_Enter);
+            this.boxCpf.Leave += new System.EventHandler(this.boxCpf_Leave);
+            // 
+            // boxSenha
+            // 
+            this.boxSenha.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxSenha.Location = new System.Drawing.Point(268, 296);
+            this.boxSenha.Multiline = true;
+            this.boxSenha.Name = "boxSenha";
+            this.boxSenha.Size = new System.Drawing.Size(263, 34);
+            this.boxSenha.TabIndex = 4;
+            this.boxSenha.Enter += new System.EventHandler(this.boxSenha_Enter);
+            this.boxSenha.Leave += new System.EventHandler(this.boxSenha_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(264, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "CPF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(264, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Senha";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(285, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(230, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Realize seu Login";
             // 
             // Telalogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(837, 473);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.boxSenha);
+            this.Controls.Add(this.boxCpf);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.roundedPictureBox1);
             this.Controls.Add(this.pictureBox1);
@@ -80,6 +148,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -88,6 +157,11 @@
         private RoundedPictureBox roundedPictureBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox boxCpf;
+        private System.Windows.Forms.TextBox boxSenha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Telalogin/view/Telalogin.cs
+++ Telalogin/view/Telalogin.cs
@@ -15,6 +15,51 @@ namespace Telalogin
         public Telalogin()
         {
             InitializeComponent();
+            boxCpf.Text = "Digite seu CPF";
+            boxCpf.ForeColor = Color.Gray;
+            boxSenha.Text = "Digite sua senha";
+            boxSenha.ForeColor = Color.Gray;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boxCpf_Enter(object sender, EventArgs e)
+        {
+            if(boxCpf.Text == "Digite seu CPF")
+            {
+                boxCpf.Text = "";
+                boxCpf.ForeColor = Color.Black;
+            }
+        }
+
+        private void boxCpf_Leave(object sender, EventArgs e)
+        {
+            if (boxCpf.Text == "")
+            {
+                boxCpf.Text = "Digite seu CPF";
+                boxCpf.ForeColor = Color.Gray;
+            }
+        }
+
+        private void boxSenha_Enter(object sender, EventArgs e)
+        {
+            if (boxSenha.Text == "Digite sua senha")
+            {
+                boxSenha.Text = "";
+                boxSenha.ForeColor = Color.Black;
+            }
+        }
+
+        private void boxSenha_Leave(object sender, EventArgs e)
+        {
+            if (boxSenha.Text == "")
+            {
+                boxSenha.Text = "Digite sua senha";
+                boxSenha.ForeColor = Color.Gray;
+            }
         }
     }
 }
