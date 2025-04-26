8f804af71bb2436866c520cb3553b0067a932c6d
lucas.fsantana7 <lucas.fsantana7@TIT00DVBAHW11-1>
2025-04-25 22:07:03 -0300

    tela de cadastro e codigo

--- Telalogin/view/TelaCadastro.Designer.cs
+++ Telalogin/view/TelaCadastro.Designer.cs
@@ -31,24 +31,28 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaCadastro));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.roundedPictureBox1 = new RoundedPictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtConfirmarSenha = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(-57, 36);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(188, 203);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -59,41 +63,19 @@
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(687, -32);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(188, 203);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(130, 342);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(188, 203);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(486, 206);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(188, 203);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
-            // 
             // roundedPictureBox1
             // 
             this.roundedPictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(74)))), ((int)(((byte)(173)))));
             this.roundedPictureBox1.CornerRadius = 30;
             this.roundedPictureBox1.Location = new System.Drawing.Point(240, 17);
-            this.roundedPictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.roundedPictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.roundedPictureBox1.Name = "roundedPictureBox1";
             this.roundedPictureBox1.Size = new System.Drawing.Size(326, 450);
             this.roundedPictureBox1.TabIndex = 4;
@@ -102,8 +84,8 @@
             // pictureBox5
             // 
             this.pictureBox5.BackColor = System.Drawing.Color.White;
-            this.pictureBox5.Location = new System.Drawing.Point(259, 36);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox5.Location = new System.Drawing.Point(260, 27);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(289, 411);
             this.pictureBox5.TabIndex = 5;
@@ -114,12 +96,71 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(267, 45);
+            this.label1.Location = new System.Drawing.Point(269, 47);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(269, 32);
             this.label1.TabIndex = 6;
             this.label1.Text = "Realize seu cadastro";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(298, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "cadastrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(275, 134);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(250, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(275, 181);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(250, 20);
+            this.txtCPF.TabIndex = 9;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(275, 230);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(250, 20);
+            this.txtEmail.TabIndex = 10;
+            this.txtEmail.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtConfirmarSenha
+            // 
+            this.txtConfirmarSenha.Location = new System.Drawing.Point(275, 317);
+            this.txtConfirmarSenha.Name = "txtConfirmarSenha";
+            this.txtConfirmarSenha.Size = new System.Drawing.Size(250, 20);
+            this.txtConfirmarSenha.TabIndex = 12;
+            this.txtConfirmarSenha.UseSystemPasswordChar = true;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(275, 276);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(250, 20);
+            this.txtSenha.TabIndex = 13;
+            this.txtSenha.UseSystemPasswordChar = true;
+            this.txtSenha.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox6.Image = global::Telalogin.Properties.Resources.fundo_2;
+            this.pictureBox6.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(824, 490);
+            this.pictureBox6.TabIndex = 14;
+            this.pictureBox6.TabStop = false;
             // 
             // TelaCadastro
             // 
@@ -127,23 +168,27 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(138)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(824, 490);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtConfirmarSenha);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.roundedPictureBox1);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "TelaCadastro";
             this.Text = "TelaCadastro";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,10 +198,15 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
         private RoundedPictureBox roundedPictureBox1;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtConfirmarSenha;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
--- Telalogin/view/TelaCadastro.cs
+++ Telalogin/view/TelaCadastro.cs
@@ -1,20 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Telalogin.view
+namespace SistemaLoginCadastro
 {
-    public partial class TelaCadastro : Form
+    public partial class MainForm : Form
     {
-        public TelaCadastro()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        // Botão para ver a agenda de consultas
+        private void btnAgendaConsultas_Click(object sender, EventArgs e)
+        {
+            // Cria uma instância da tela de agenda de consultas
+            AgendaConsultasForm agendaForm = new AgendaConsultasForm();
+            agendaForm.Show();
+            this.Hide();  // Fecha o formulário principal
+        }
+
+        // Botão para ver os medicamentos
+        private void btnMedicamentos_Click(object sender, EventArgs e)
+        {
+            // Cria uma instância da tela de medicamentos
+            MedicamentosForm medicamentosForm = new MedicamentosForm();
+            medicamentosForm.Show();
+            this.Hide();  // Fecha o formulário principal
+        }
+
+        // Botão para agendar uma nova consulta
+        private void btnAgendarConsulta_Click(object sender, EventArgs e)
+        {
+            // Cria uma instância da tela de agendamento de consulta
+            AgendarConsultaForm agendarForm = new AgendarConsultaForm();
+            agendarForm.Show();
+            this.Hide();  // Fecha o formulário principal
+        }
+
+        // Botão para acessar consultas, prescrições e atestados
+        private void btnConsultasPrescricoes_Click(object sender, EventArgs e)
+        {
+            // Cria uma instância da tela de consultas, prescrições e atestados
+            ConsultasPrescricoesForm consultasForm = new ConsultasPrescricoesForm();
+            consultasForm.Show();
+            this.Hide();  // Fecha o formulário principal
+        }
+
+        // Botão para acessar pedidos médicos
+        private void btnPedidosMedicos_Click(object sender, EventArgs e)
+        {
+            // Cria uma instância da tela de pedidos médicos
+            PedidosMedicosForm pedidosForm = new PedidosMedicosForm();
+            pedidosForm.Show();
+            this.Hide();  // Fecha o formulário principal
+        }
+
+        // Botão para voltar ao início (login)
+        private void btnVoltarInicio_Click(object sender, EventArgs e)
+        {
+            // Cria uma instância da tela de login e volta para ela
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Close();  // Fecha o formulário atual (MainForm)
         }
     }
 }
