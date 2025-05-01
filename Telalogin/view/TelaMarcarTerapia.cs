ff6e419b843f8779bc1461c264a799d49fddd25a
Guilherme <lima.guilhermeramos@gmail.com>
2025-05-01 18:55:11 -0300

    modificando textos

---
+++ Telalogin/view/TelaMarcarTerapia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Telalogin.view
+{
+    public partial class TelaMarcarTerapia : Form
+    {
+        public TelaMarcarTerapia()
+        {
+            InitializeComponent();
+        }
+    }
+}
