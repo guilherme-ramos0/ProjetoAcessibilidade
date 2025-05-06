ae3d3d9aa3cf8332b163b58a9c93c288eea157f5
guilherme-ramos0 <lima.guilhermeramos@gmail.com>
2025-05-05 22:21:34 -0300

    debug - colocando dados para conexao

---
+++ Telalogin/model/Regra/Regra.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaoMySQL.Model.Regra
+{
+    public class Regra
+    {
+        public int idregra { get; set; }
+        public string nomeRegra { get; set; }
+    }
+}
---
+++ Telalogin/model/Usuario/Usuario.cs
@@ -0,0 +1,42 @@
+﻿using ConexaoMySQL.Model.Regra;
+using MySql.Data.MySqlClient;
+using System;
+
+
+namespace ConexaoMySQL.Models
+{
+    class Usuario
+    {
+        public int IdCadastro { get; set; }
+        public string NomeCompleto { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string HashSenha { get; set; }
+        public string Cpf { get; set; }
+        public int Zonas_idZonas { get; set; }
+        public Regra Regra { get; set; }
+
+        public static Usuario UserFromDataReader(MySqlDataReader reader)
+        {
+
+            return new Usuario
+            {
+                IdCadastro = Convert.ToInt32(reader["idcadastro"]),
+                NomeCompleto = reader["NomeCompleto"].ToString(),
+                Telefone = reader["Telefone"].ToString(),
+                DataNascimento = Convert.ToDateTime(reader["DataNascimento"]),
+                HashSenha = reader["Senha"].ToString(),
+                Cpf = reader["Cpf"].ToString(),
+                Zonas_idZonas = Convert.ToInt32(reader["Zonas_idZonas"]),
+                Regra = new Regra
+                {
+                    idregra = Convert.ToInt32(reader["idregra"].ToString()),
+                    nomeRegra = reader["nomeRegra"].ToString()
+                }
+
+            };
+
+
+        }
+    }
+}
