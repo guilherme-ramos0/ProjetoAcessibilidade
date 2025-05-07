80e0505bf33303ebdd2478fd1ab19b4686a8c5fb
Guilherme <lima.guilhermeramos@gmail.com>
2025-05-07 13:56:28 -0300

    montando login

--- Telalogin/view/loginUsuario.Designer.cs
+++ Telalogin/view/loginUsuario.Designer.cs
@@ -30,6 +30,16 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginUsuario));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.bordaredonda3 = new Bordaredonda();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.bordaredonda4 = new Bordaredonda();
+            this.btnCadastrar = new BotaoBordaArredondada();
+            this.login = new BotaoBordaArredondada();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.bordaredonda1 = new Bordaredonda();
             this.bordaredonda2 = new Bordaredonda();
             this.label1 = new System.Windows.Forms.Label();
@@ -40,6 +50,9 @@
             this.txtSenha = new System.Windows.Forms.Label();
             this.txtCPF = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
+            this.bordaredonda3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.bordaredonda4.SuspendLayout();
             this.bordaredonda1.SuspendLayout();
             this.bordaredonda2.SuspendLayout();
             this.SuspendLayout();
@@ -50,15 +63,139 @@
             this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            //this.tableLayoutPanel1.Controls.Add(this.bordaredonda1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.bordaredonda3, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(921, 580);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1010, 617);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // bordaredonda3
+            // 
+            this.bordaredonda3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bordaredonda3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(74)))), ((int)(((byte)(173)))));
+            this.bordaredonda3.Controls.Add(this.tableLayoutPanel2);
+            this.bordaredonda3.CornerRadius = 80;
+            this.bordaredonda3.ForeColor = System.Drawing.Color.White;
+            this.bordaredonda3.Location = new System.Drawing.Point(146, 39);
+            this.bordaredonda3.Name = "bordaredonda3";
+            this.bordaredonda3.Size = new System.Drawing.Size(717, 538);
+            this.bordaredonda3.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.bordaredonda4, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(717, 538);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // bordaredonda4
+            // 
+            this.bordaredonda4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bordaredonda4.BackColor = System.Drawing.Color.White;
+            this.bordaredonda4.Controls.Add(this.btnCadastrar);
+            this.bordaredonda4.Controls.Add(this.login);
+            this.bordaredonda4.Controls.Add(this.textBox1);
+            this.bordaredonda4.Controls.Add(this.maskedTextBox1);
+            this.bordaredonda4.Controls.Add(this.label4);
+            this.bordaredonda4.Controls.Add(this.label3);
+            this.bordaredonda4.Controls.Add(this.label2);
+            this.bordaredonda4.CornerRadius = 80;
+            this.bordaredonda4.Location = new System.Drawing.Point(40, 29);
+            this.bordaredonda4.Name = "bordaredonda4";
+            this.bordaredonda4.Size = new System.Drawing.Size(637, 480);
+            this.bordaredonda4.TabIndex = 0;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.AutoSize = true;
+            this.btnCadastrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(74)))), ((int)(((byte)(173)))));
+            this.btnCadastrar.CornerRadius = 20;
+            this.btnCadastrar.FlatAppearance.BorderSize = 0;
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrar.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(352, 386);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(160, 38);
+            this.btnCadastrar.TabIndex = 6;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            // 
+            // login
+            // 
+            this.login.AutoSize = true;
+            this.login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(74)))), ((int)(((byte)(173)))));
+            this.login.CornerRadius = 20;
+            this.login.FlatAppearance.BorderSize = 0;
+            this.login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login.Location = new System.Drawing.Point(100, 386);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(176, 38);
+            this.login.TabIndex = 5;
+            this.login.Text = "Realizar login";
+            this.login.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(187, 287);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 34);
+            this.textBox1.TabIndex = 4;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(187, 194);
+            this.maskedTextBox1.Mask = "000.000.000-00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(198, 34);
+            this.maskedTextBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(181, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 32);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Senha";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(181, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 32);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "CPF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(261, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 38);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Login";
             // 
             // bordaredonda1
             // 
@@ -95,7 +232,7 @@
             this.label1.Font = new System.Drawing.Font("Arial Black", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(52, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(291, 48);
+            this.label1.Size = new System.Drawing.Size(281, 46);
             this.label1.TabIndex = 15;
             this.label1.Text = "Faça seu login";
             // 
@@ -114,7 +251,6 @@
             this.btnLogin.TabIndex = 14;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = false;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click_1);
             // 
             // btnCadastro
             // 
@@ -132,7 +268,6 @@
             this.btnCadastro.TabIndex = 13;
             this.btnCadastro.Text = "Cadastro";
             this.btnCadastro.UseVisualStyleBackColor = false;
-            this.btnCadastro.Click += new System.EventHandler(this.btnCadastro_Click);
             // 
             // txtBoxCpf
             // 
@@ -180,10 +315,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(921, 580);
+            this.ClientSize = new System.Drawing.Size(1010, 617);
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "loginUsuario";
@@ -191,6 +326,10 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "loginUsuario";
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.bordaredonda3.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.bordaredonda4.ResumeLayout(false);
+            this.bordaredonda4.PerformLayout();
             this.bordaredonda1.ResumeLayout(false);
             this.bordaredonda2.ResumeLayout(false);
             this.bordaredonda2.PerformLayout();
@@ -210,5 +349,15 @@
         private BotaoBordaArredondada btnCadastro;
         private BotaoBordaArredondada btnLogin;
         private System.Windows.Forms.Label label1;
+        private Bordaredonda bordaredonda3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private Bordaredonda bordaredonda4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private BotaoBordaArredondada btnCadastrar;
+        private BotaoBordaArredondada login;
     }
 }
